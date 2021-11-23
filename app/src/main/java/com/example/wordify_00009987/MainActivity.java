a18d5805760f8a530a7cddac13dfd03b648b33d4
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-23 10:49:38 +0500

    init

---
+++ app/src/main/java/com/example/wordify_00009987/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wordify_00009987;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
