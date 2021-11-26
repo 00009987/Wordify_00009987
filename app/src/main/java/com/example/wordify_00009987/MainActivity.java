7794fd7acae0644609c791fd6967407a4304276a
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-26 12:37:53 +0500

    add sample data on app launch

--- app/src/main/java/com/example/wordify_00009987/MainActivity.java
+++ app/src/main/java/com/example/wordify_00009987/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.wordify_00009987;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.provider.UserDictionary;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -13,11 +17,43 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // populating the sample data on app launch
+        populateSampleData();
+    }
+
+    private void populateSampleData() {
+        // connect to db
+        DictionaryDbManager dbManager = new DictionaryDbManager(this);
+        SQLiteDatabase db = dbManager.getReadableDatabase();
+        Cursor dictionary = db.query("dictionary", null, null, null, null, null, null);
+
+        if (dictionary.getCount() == 0) {
+            // sample data
+            String[] originalWords = new String[]{"言葉", "diccionario", "удивительный"};
+            String[] translations = new String[]{"word", "dictionary", "amazing"};
+            String[] definitions = new String[]{"a single distinct meaningful element of speech or writing, used with others (or sometimes alone) to form a sentence and typically shown with a space on either side when written or printed.", "gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage", "causing great surprise or wonder; astonishing"};
+            String[] languages = new String[]{"japanese", "spanish", "russian"};
+            Boolean[] isFavorites = new Boolean[]{true, false, false};
+            Boolean[] isArchives = new Boolean[]{false, false, true};
+
+            // insert samplate data into db
+            ContentValues values = new ContentValues();
+
+            for (int i = 0; i < originalWords.length; i++) {
+                values.put("originalWord", originalWords[i]);
+                values.put("translation", translations[i]);
+                values.put("definition", definitions[i]);
+                values.put("language", languages[i]);
+                values.put("isFavorite", isFavorites[i]);
+                values.put("isArchived", isArchives[i]);
+                db.insert("dictionary", null, values);
+            }
+        }
     }
 
     public void openAboutActivity(View view) {
