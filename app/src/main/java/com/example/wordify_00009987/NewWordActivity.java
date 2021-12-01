3e9f7b8872a479aad30649b15327f2f100eb80c5
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-12-01 11:07:12 +0500

    add filter ui

--- app/src/main/java/com/example/wordify_00009987/NewWordActivity.java
+++ app/src/main/java/com/example/wordify_00009987/NewWordActivity.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -122,7 +119,7 @@ public class NewWordActivity extends AppCompatActivity {
         });
     }
 
-    private void generateSpinnerOptions() {
+    public void generateSpinnerOptions() {
         Spinner dropdown = findViewById(R.id.language_spinner);
 
         // basic adapter to describe how the items are displayed
--- app/src/main/java/com/example/wordify_00009987/WordsListActivity.java
+++ app/src/main/java/com/example/wordify_00009987/WordsListActivity.java
@@ -7,19 +7,28 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 public class WordsListActivity extends AppCompatActivity {
-    private Cursor dictionary;
+    private final String[] options = new String[]{"spanish", "japanese", "german", "russian"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words_list);
 
+        onWordListStart();
+        generateSpinnerOptions();
+    }
+
+    private void onWordListStart() {
         // connect to db
         DictionaryDbManager dbManager = new DictionaryDbManager(this);
         SQLiteDatabase db = dbManager.getReadableDatabase();
+        Cursor dictionary = null;
 
         // get type of the word list
         Intent i = getIntent();
@@ -46,5 +55,19 @@ public class WordsListActivity extends AppCompatActivity {
         DictionaryAdapter dcAdapter = new DictionaryAdapter(this, dictionary, type);
         ListView listView = findViewById(R.id.words_list);
         listView.setAdapter(dcAdapter);
+    }
+
+    public void generateSpinnerOptions() {
+        Spinner dropdown = findViewById(R.id.filter_spinner);
+
+        // basic adapter to describe how the items are displayed
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_style, options);
+        dropdown.setAdapter(adapter);
+    }
+
+    public void filterByLanguage(View view) {
+        Spinner filterSpinner = findViewById(R.id.filter_spinner);
+        String selectedLanguage = filterSpinner.getSelectedItem().toString();
+        Log.d("language", selectedLanguage);
     }
 }
